45e1eef0a440a4b5a2b66e58f46f114a95075461
Mog.Lu <mog.lu@vechain.com>
2020-12-29 15:29:04 +0800

    update error map

--- src/app/initErrorMap.ts
+++ src/app/initErrorMap.ts
@@ -3,5 +3,5 @@ import { errorMap } from "../utils/middleware/convertJSONResponeMiddleware";
 
 export const initErrorMap = function(){
     errorMap.set(SystemDefaultError.INTERNALSERVERERROR,{status:500,err:{error:SystemDefaultError.INTERNALSERVERERROR.message,error_detail:""}})
-    errorMap.set(SystemDefaultError.BADREQUEST,{status:400,err:{error:SystemDefaultError.INTERNALSERVERERROR.message,error_detail:""}})
+    errorMap.set(SystemDefaultError.BADREQUEST,{status:400,err:{error:SystemDefaultError.BADREQUEST.message,error_detail:""}})
 }
